09463f3cfeb251f192c41f9a6851d6e9911f7e4c
Girjev Diana <diana.girjev@gmail.com>
2022-03-12 17:12:34 +0200

    Commit_2 : Winning Button is chosen before we click those 3 buttons

--- GuessButton.js
+++ GuessButton.js
@@ -1,8 +1,13 @@
-function checkWinningButton(button) {
+function generateWinningButton() {
   let winningButton = 1 + Math.floor(Math.random() * 3);
+  return winningButton;
+}
+
+function checkWinningButton(button, winningButton) {
   if (button == winningButton) {
     alert("YOU WON THE GAME - Congratulation !!!");
   } else {
     alert("YOU LOST THE GAME - Try Again !!!");
   }
+  return false;
 }
