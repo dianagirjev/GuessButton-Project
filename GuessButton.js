85e31dd58ee1cba5803736da858f14ea24ca66ee
Girjev Diana <diana.girjev@gmail.com>
2022-03-15 23:07:16 +0200

    Commit_3: Winning button is generated every 10 seconds

--- GuessButton.js
+++ GuessButton.js
@@ -1,5 +1,8 @@
+setInterval(winningButton = generateWinningButton,10000);
+
 function generateWinningButton() {
-  let winningButton = 1 + Math.floor(Math.random() * 3);
+  winningButton = 1 + Math.floor(Math.random() * 3);
+  alert("Hurry Up!!! The winning button will change after 10 seconds from now on.");
   return winningButton;
 }
 
